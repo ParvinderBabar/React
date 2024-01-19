4acc5351e8ca801c0e208c7f6e23e261f30b4c29
parvinderBabar <er.kaurparvinder@gmail.com>
2024-01-19 16:49:00 -0500

    commit my-app basics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-
+let projectName = "foodRecepie";
 function App() {
   return (
     // <div className="App">
@@ -18,7 +18,25 @@ function App() {
     //     </a>
     //   </header>
     // </div>
-    <div className="blank">EMMET</div>
+    <>
+      <div className="main">
+        <h1> {projectName}</h1>
+      </div>
+      <div className="container">
+        <nav>
+          <li>HOME</li>
+          <li>ABOUT</li>
+          <li>CONTACT</li>
+        </nav>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+          voluptas quam corporis cum magnam quas aliquam perspiciatis explicabo
+          aspernatur. Architecto quod saepe, sunt est corrupti dolores
+          accusantium laboriosam adipisci laudantium.
+        </p>
+        <h1> babel compiles jsx down to react.createElement() calls</h1>
+      </div>
+    </>
   );
 }
 
